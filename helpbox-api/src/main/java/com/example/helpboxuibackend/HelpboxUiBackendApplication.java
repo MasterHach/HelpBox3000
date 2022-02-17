ac5023f478ef18be2cd20e856ec70bbda4aaf87f
F0ll0w-Th3-Dr3am <pesdavyd@gmail.com>
2022-02-18 00:50:59 +0300

    Добавлена базовая разметка проекта

---
+++ helpbox-api/src/main/java/com/example/helpboxuibackend/HelpboxUiBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.helpboxuibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HelpboxUiBackendApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpboxUiBackendApplication.class, args);
+	}
+
+}
---
+++ helpbox-api/src/test/java/com/example/helpboxuibackend/HelpboxUiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helpboxuibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpboxUiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
