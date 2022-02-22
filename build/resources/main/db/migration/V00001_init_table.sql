73fae7150f3f3192054dd19e4e940d0633eb7416
nmikhin <nmikhin@digital-spirit.ru>
2022-02-23 00:00:42 +0300

    Исправлена механика авторизации пользователей

---
+++ build/resources/main/db/migration/V00001_init_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Users
+(
+    id SERIAL,
+    email VARCHAR(120) PRIMARY KEY,
+    password VARCHAR(255),
+    name VARCHAR(32),
+    surname VARCHAR(32),
+    role VARCHAR(10)
+);
